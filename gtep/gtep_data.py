971e96e22525e4b6daeee3ec83b29d6f87bba883
Kyle Skolfield <jkskolf@sandia.gov>
2025-07-03 14:40:54 -0600

    transmission cost defaults

--- gtep/gtep_data.py
+++ gtep/gtep_data.py
@@ -78,7 +78,7 @@ class ExpansionPlanningData:
 
         ## RMA:
         ## Change the last date for whatever extreme day is needed based on the given run(s)
-        
+
         time_keys = self.md.data["system"]["time_keys"]
         self.representative_dates = [
             "2020-01-17 00:00",
@@ -199,6 +199,7 @@ class ExpansionPlanningData:
         for branch in self.md.data["elements"]["branch"]:
             self.md.data["elements"]["branch"][branch]["loss_rate"] = 0
             self.md.data["elements"]["branch"][branch]["distance"] = 1
+            self.md.data["elements"]["branch"][branch]["capital_cost"] = 10000000
         self.md.data["system"]["min_operating_reserve"] = 0.1
         self.md.data["system"]["min_spinning_reserve"] = 0.1
 
