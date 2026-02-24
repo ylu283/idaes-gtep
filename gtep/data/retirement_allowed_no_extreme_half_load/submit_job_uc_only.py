4a56686b721e03329b87f0c795c220b57b77ffd6
ylu283 <139079568+ylu283@users.noreply.github.com>
2026-02-24 12:38:00 -0500

    submit job and submit job uc
    update network type in baseline case to ptdf. simulation for 90 days
    create new prescient options and submit job with no ed. simulation for 90 days

---
+++ gtep/data/retirement_allowed_no_extreme_half_load/submit_job_uc_only.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from textwrap import dedent
+import subprocess
+
+THIS_DIR = Path(__file__).resolve().parent
+
+def submit_job(job_name: str = "ERCOT") -> None:
+    # make job_scripts dir
+    job_scripts_dir = THIS_DIR / "job_scripts"
+    os.makedirs(job_scripts_dir, exist_ok=True)
+
+    # write the shell script
+    sh_path = job_scripts_dir / f"{job_name}.sh"
+    script = dedent(f"""\
+        #!/bin/bash
+        #$ -M ylu28@nd.edu
+        #$ -m ae
+        #$ -q long
+        #$ -N {job_name}
+
+        # ensure conda is available
+        source ~/.bashrc
+        conda activate /users/ylu28//dispatches
+
+        export LD_LIBRARY_PATH=~/.conda/envs/users/ylu28//dispatches/lib:$LD_LIBRARY_PATH
+        module load gurobi
+        module load ipopt/3.14.2
+
+        python ./run_coal_prescient.py
+    """)
+    with open(sh_path, "w", newline="\n") as f:
+        f.write(script)
+    os.chmod(sh_path, 0o755)  # optional
+
+    subprocess.run(["qsub", str(sh_path)], check=True)
+    print(f"Submitted: {sh_path}")
+
+if __name__ == "__main__":
+    submit_job("ERCOT")
