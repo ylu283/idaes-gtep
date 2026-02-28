5e493e3e73fda3644306b2ae83b27ee4ab50ee50
ylu283 <ylu28@nd.edu>
2026-02-27 21:30:07 -0500

    update with hydro configurations
    update crc job settings for uced, uconly jobs with btheta and 90days

---
+++ gtep/data/retirement_allowed_no_extreme_half_load/run_coal_prescient_btheta.py
@@ -0,0 +1,32 @@
+from prescient.simulator import Prescient
+
+# btheta UC+ED configuration with hydro generators
+prescient_options = {
+    "data_path": "Prescient_2",
+    "input_format": "rts-gmlc",
+    "simulate_out_of_sample": True,
+    "run_sced_with_persistent_forecast_errors": True,
+    "output_directory": "Prescient_2/results_btheta",
+    "start_date": "01-01-2019",
+    "num_days": 90,
+    "sced_horizon": 6,
+    "ruc_mipgap": 0.01,
+    "deterministic_ruc_solver": "gurobi_persistent",
+    "deterministic_ruc_solver_options": "TimeLimit=3600",
+    "sced_solver": "gurobi",
+    "sced_frequency_minutes": 60,
+    "ruc_horizon": 36,
+    "compute_market_settlements": True,
+    "monitor_all_contingencies": False,
+    "output_solver_logs": False,
+    "price_threshold": 1000,
+    "contingency_price_threshold": 100,
+    "reserve_price_threshold": 5,
+    "ruc_network_type": "btheta",
+    "sced_network_type": "btheta",
+    "enforce_sced_shutdown_ramprate": False,
+    "reserve_factor": 0.1,
+    "day_ahead_pricing": "LMP",
+}
+# run the simulator
+Prescient().simulate(**prescient_options)
